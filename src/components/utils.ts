9c8c7305eb71dc2eb240041ab3e9ef2229fe059b
Nikita Gawde <gawdenikita10@gmail.com>
2025-12-11 14:36:19 +0530

    adding more gifs

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -12,6 +12,11 @@ export const GIF_POOL: Record<
     },
     {
       //jungkook
+      src: "https://i.pinimg.com/originals/12/e4/ef/12e4ef95f14883e45d85608a89449ec4.gif",
+      alt: "Jungkook happy",
+    },
+    {
+      //jungkook
       src: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXRoM2l0cHl3bTRlaThjd2V5OWRyejN4aGx5anlqZWdoOGV6ZzRkeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gKGLm7ksfrqtKRQ0Sy/giphy.gif",
       alt: "Approved",
     },
@@ -40,13 +45,48 @@ export const GIF_POOL: Record<
       src: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2ZsMnAzanQycHB2NG9oYXc3cTVpa3V1a2V6OWNzNHNtdXB0d3d2cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0QqZfsK8q4KxUvNMxZ/giphy.gif",
       alt: "Excited yes",
     },
+    {
+      //V
+      src: "https://media1.tenor.com/m/FyzVdOQYW3oAAAAd/tae-taehyung.gif",
+      alt: "Taehyung happy",
+    },
+    {
+      //V
+      src: "https://media1.tenor.com/m/GEF2by0Hmw4AAAAd/taehyung-taehyung-layover.gif",
+      alt: "V happy",
+    },
+    {
+      //Taekook
+      src: "https://media1.tenor.com/m/myZEifI7Et8AAAAC/dynataees-taekook.gif",
+      alt: "Taekook happy",
+    },
+    {
+      //V
+      src: "https://media1.tenor.com/m/cpxI6ngyuOUAAAAd/taehyung-eat.gif",
+      alt: "V eating happily",
+    },
   ],
 
   okay_in_moderation: [
     {
+      //jikook
+      src: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3aDk5ZzVsbHJudGJnM3g1ZDFqanJxcGt4MmQ2NWoyeXNwd2pjbGFiaiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ibYI5m2EyLgEnanuid/giphy.gif",
+      alt: "Thinking",
+    },
+    {
+      //junkook
+      src: "https://media1.tenor.com/m/Lu-UIDEWJ8QAAAAd/bts-bangtan-boys.gif",
+      alt: "Jungkook cautious",
+    },
+    {
       //uday
       src: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHR4YnB3azhsdng1eHBseTlqZno3OXNxM3pldHZ2dGFkYjh0ZjY1ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XBpEgheIVWnC8hwc46/giphy.gif",
       alt: "Control",
+    },
+    {
+      //jikook
+      src: "https://media1.tenor.com/m/h7O7Zc8nWP8AAAAC/park-jimin-jimin.gif",
+      alt: "Jikook IDK",
     },
     {
       //jimin
@@ -57,6 +97,11 @@ export const GIF_POOL: Record<
       //v
       src: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGk1dTNiZDJsYzFzeHNyMDVzY3ZrM3JycDZjeDRzZmh5MmgxZHpoNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UurFWROK7ZMvKLNvio/giphy.gif",
       alt: "Think first",
+    },
+    {
+      //V
+      src: "https://media1.tenor.com/m/of7_B5Ta4LgAAAAC/taehyung-bts.gif",
+      alt: "V thinking",
     },
     {
       //jungkook
@@ -111,6 +156,26 @@ export const GIF_POOL: Record<
       src: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzg0aXQwd2Jwc3g0NHB1NXJvOGxrN2RxejZodzZ5YzhlaXl3Zm1rdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d2sTZOZQNVAOIXs3Vv/giphy.gif",
       alt: "Absolutely not",
     },
+    {
+      //V
+      src: "https://media1.tenor.com/m/Xb_qGF5cPpAAAAAd/taehyung-pout-bts-pout.gif",
+      alt: "V disapproves",
+    },
+    {
+      //jungkook
+      src: "https://media1.tenor.com/m/90vjEn3wd3oAAAAd/bts-jungkook.gif",
+      alt: "Jungkook disapproves",
+    },
+    {
+      //V
+      src: "https://media1.tenor.com/m/1mnhB_bltb0AAAAd/taehyungtaehyung-taehyung.gif",
+      alt: "V says no",
+    },
+    {
+      //jimin
+      src: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3b2gxMmh4dzZ2bXJtcGhlY3l4NXplcnRwMWVteDQxdnZqdDZkZzEyciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/WNXT4fxjlLv2Ez9gUn/giphy.gif",
+      alt: "Jimin disapproves",
+    },
   ],
 
   unknown: [
@@ -118,6 +183,16 @@ export const GIF_POOL: Record<
       //jhope
       src: "https://media.giphy.com/media/ZTFgbsUU0NRYI/giphy.gif",
       alt: "Confused",
+    },
+    {
+      //jungkook
+      src: "https://media1.tenor.com/m/JjJNF4CTerEAAAAd/jeonvntold.gif",
+      alt: "Jungkook confused",
+    },
+    {
+      //V
+      src: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3dmF2bXB5MXRocTZlbGRzNHNhaTlyZDB6OHI2cGkyMmt6ZWttcTh4dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/PmpenkvG0JmiQ/giphy.gif",
+      alt: "V confused",
     },
   ],
 };
